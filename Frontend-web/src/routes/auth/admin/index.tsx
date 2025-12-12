e57e68021353f9a1671452508efabd6c2189d4df
Shaon An Nafi <sarkarnafe@gmail.com>
2025-12-12 12:24:50 +0600

    feat: remove unused TableCaption import and enhance quiz room creation functionality

--- Frontend-web/src/routes/auth/admin/index.tsx
+++ Frontend-web/src/routes/auth/admin/index.tsx
@@ -2,7 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
--- Frontend-web/src/routes/quiz/room/create/index.tsx
+++ Frontend-web/src/routes/quiz/room/create/index.tsx
@@ -1,17 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { createFileRoute } from "@tanstack/react-router";
+import { Shuffle } from "lucide-react";
+import { useState } from "react";
 
 export const Route = createFileRoute("/quiz/room/create/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [roomid, setRoomid] = useState("");
+
+  const createRoomID = () => {
+    const uuid = crypto.randomUUID();
+    setRoomid(uuid.split("-")[0]);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <p>Enter the name of your quiz room:</p>
       <div className="flex flex-row gap-4">
-        <Input type="text" placeholder="Room Code" />
+        <Input type="text" placeholder="Room Code" value={roomid}></Input>
+        <Button
+          variant={"outline"}
+          onClick={() => {
+            createRoomID();
+          }}
+        >
+          <Shuffle />
+        </Button>
         <Button variant={"outline"}>Create Room</Button>
       </div>
     </div>
